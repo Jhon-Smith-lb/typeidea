47f8baeb9a94b7f996e47264460e49cdb136a466
lb <lb@localhost.localdomain>
2020-01-09 14:51:44 +0800

    Model工作完成

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -2,7 +2,7 @@ from .base import * # NOQA
 
 DEBUG = True
 
-TABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
